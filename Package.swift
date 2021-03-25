c7ec57c9b3a8f85a39b16de08fb9084a71f1f88c
Soares e Neves <soareseneves@terra.com.br>
2021-03-25 11:58:25 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,22 @@
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
-    name: "PDFReader"
+    name: "PDFReader",
+    platforms: [
+        .iOS("8.0")
+    ],
+    products: [
+        .library(name: "PDFReader", targets: ["PDFReader"])
+    ],
+    targets: [
+        .target(
+            name: "PDFReader",
+            path: "PDFReader",
+            exclude: [
+                "Info.plist",
+            ],
+            resources: [.process("Resources")]
+        )
+    ]
 )
