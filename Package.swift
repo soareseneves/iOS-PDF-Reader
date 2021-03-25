86ee3ba8cec1c57f34ca2b75d1a9defafea3e73e
Soares e Neves <soareseneves@terra.com.br>
2021-03-25 15:24:32 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "PDFReader",
     defaultLocalization: "en",
     platforms: [
-        .iOS("8.0")
+        .iOS("9.0")
     ],
     products: [
         .library(name: "PDFReader", targets: ["PDFReader"])
